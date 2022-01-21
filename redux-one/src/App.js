6537c8b8b9350a2397138a138de93c9109320971
rasel <russelroy15@gmail.com>
2022-01-22 00:13:15 +0600

    WIP on Redux intermediate project

--- redux-one/src/App.js
+++ redux-one/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <div className="container text-center my-5">
         <h2 className="text-secondary fw-bold">Increment/Decrement counter</h2>
         <h4 className="text-secondary fw-bold mb-4">using React and Redux</h4>
-        <container className="mt-4">
+        <div className="mt-4 container">
           <Button variant="outline-secondary">+</Button>
           <input
             type="text"
@@ -17,7 +17,7 @@ function App() {
             style={{ width: "33px", height: "33px", borderRadius: "5px" }}
           />
           <Button variant="outline-secondary">-</Button>
-        </container>
+        </div>
       </div>
     </>
   );
