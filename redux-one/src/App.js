529213097cf404376d6d2e5208d8a333fed18fc0
rasel <russelroy15@gmail.com>
2022-01-21 16:56:33 +0600

    WIP on Project one Redux app

--- redux-one/src/App.js
+++ redux-one/src/App.js
@@ -1,11 +1,17 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Container } from "react-bootstrap";
 import "./App.css";
 
 function App() {
   return (
     <>
       <div className="container text-center my-5">
-        <h2 className="text-info">This is Project One</h2>
+        <h2 className="text-secondary fw-bold">Increment/Decrement counter</h2>
+        <h4 className="text-secondary fw-bold">using React and Redux</h4>
+        <Container className="my-3">
+          <Button variant="outline-secondary">+</Button>
+          <Button variant="outline-secondary">-</Button>
+        </Container>
       </div>
     </>
   );
